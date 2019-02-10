e18140fc8763c01458267be3d817bfb9284b2d46
Lani Heritage <lelani.heritage@hotmail.co.uk>
2019-02-10 18:57:00 +0000

    Initial commit

---
+++ src/tests/components/ExpenseListItem.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import expenses from '../fixtures/expenses';
+import ExpenseListItem from '../../components/ExpenseListItem';
+
+test('should render ExpenseListItem correctly', () => {
+  const wrapper = shallow(<ExpenseListItem {...expenses[0]} />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('should render ExpenseListItem correctly',()=>{
+const wrapper =shallow(<ExpenseListItem  {...expenses[1]}/>);
+expect(wrapper).toMatchSnapshot();
+});
